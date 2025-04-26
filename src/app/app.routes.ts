e410a67bbaa47d64df30b96032732f70c90c27c6
JP <jplmarotinho22@gmail.com>
2025-04-26 12:09:21 -0300

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Página inicial',
+    loadComponent: () => import('./home-component/home-component.component').then(m => m.HomeComponentComponent),
+  },
+  {
+    path: 'products',
+    title: 'Produtos',
+    loadComponent: () => import('./products/products.component').then(m => m.ProductsComponent),
+  },
+  {
+    path: 'products/:id',
+    title: 'Detalhes do produto',
+    loadComponent: () => import('./product-details/product-details.component').then(m => m.ProductDetailsComponent)
+  }
+];
